99444173f53f02006a4c04e2997646ccdc1fc809
Winston Wiradi <winstonwiradip@gmail.com>
2020-04-15 14:20:29 +0700

    Updates

---
+++ precache-manifest.76986628a6974fc91bbfb8e2c9862742.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dadfb058cf9590e32ac0d62c623a14c8",
+    "url": "/utsweb-16123/index.html"
+  },
+  {
+    "revision": "fa3cd281e64d1e4b56c2",
+    "url": "/utsweb-16123/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "9abbcd30eb9edf29b689",
+    "url": "/utsweb-16123/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fa3cd281e64d1e4b56c2",
+    "url": "/utsweb-16123/static/js/2.fe684f2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/utsweb-16123/static/js/2.fe684f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9abbcd30eb9edf29b689",
+    "url": "/utsweb-16123/static/js/main.3667af6d.chunk.js"
+  },
+  {
+    "revision": "a79ba4d3d77fc61769bd",
+    "url": "/utsweb-16123/static/js/runtime-main.b3f299e6.js"
+  }
+]);
